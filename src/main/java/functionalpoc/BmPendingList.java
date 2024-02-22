fa3eeaaa5f48a64f55f724b7ff948a2fb13fcf52
Binesh Michael <bineshmichael@BLRSFLT225.local>
2024-02-22 18:08:29 +0530

    new classess added

---
+++ src/main/java/functionalpoc/BmPendingList.java
@@ -0,0 +1,20 @@
+package functionalpoc;
+
+import lombok.Data;
+
+@Data
+public class BmPendingList {
+	
+	
+	private String kmName;
+	private String KmGfsId;
+	private String kmBranch;
+	private String BmName;
+	private String BmGfsid;
+	private String BmBranch;
+	private String blockRemark;
+	private String UpdatedOn; 
+	private String CreatedOn;
+	private String status;
+
+}
